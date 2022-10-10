586fea2ebc108f49dd1fc4a91dff4d85779da7f4
Tarik Doganay <99910240+GoodGuyForReal@users.noreply.github.com>
2022-10-10 13:41:09 +0200

    d

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <nav className='nav'>
-       <link href='#'>TRVL</link>
-       wbiqbqwbi
+      <a href='#' className='nav-logo'>Nav</a>
     </nav>
   )
 }
