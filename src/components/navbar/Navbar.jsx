8cbb7b43710d34b5ad0f9f38642627cb66f24c2b
Tarik Doganay <99910240+GoodGuyForReal@users.noreply.github.com>
2022-10-10 14:25:25 +0200

    d

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
 function Navbar() {
-  return (
-    <nav className='nav'>
-      <a href='#' className='nav-logo'>Nav</a>
-    </nav>
-  )
+    return (
+        <nav className='nav'>
+            <a href='#' className='nav-logo'>TRVL</a>
+            <ul>
+                <li className='active'>
+                    <a href='#' className='Home'>Home</a>
+                </li>
+                <li>
+                    <a href='#' className='Home'>About Us</a>
+                </li>
+                <li>
+                    <a href='#' className='Home'>Pricing</a>
+                </li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar
