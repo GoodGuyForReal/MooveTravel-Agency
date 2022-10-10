6ac28cbd3be996e00679fac0e9ea1cd937685703
Tarik Doganay <99910240+GoodGuyForReal@users.noreply.github.com>
2022-10-10 18:13:23 +0200

    n

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 
 import './App.css';
-import {Button , PrButton} from './components/button/Button.jsx';
+import { Button, PrButton } from './components/button/Button.jsx';
 import Navbar from './components/navbar/Navbar.jsx';
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Button/>
-      <PrButton/>
-      <div>iugiufiwqfiu</div>
+      <Navbar />
+      <section className='hero'>
+        <div className='hero content'>
+          <h1>ADVANTURE AWAITS</h1>
+          <h4>What are you waiting for?</h4>
+          <div className='btns'>
+            <Button />
+            <PrButton />
+          </div>
+        </div>
+      </section>
+
+
     </div>
   );
 }
