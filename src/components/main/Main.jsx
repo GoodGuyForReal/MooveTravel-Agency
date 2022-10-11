3ec89a4176624ce4f5c874e52df1ef76e90e27f3
Tarik Doganay <99910240+GoodGuyForReal@users.noreply.github.com>
2022-10-11 14:37:26 +0200

    n

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -4,9 +4,17 @@ import Mainstyle from './Main.module.css'
 
 const Main = () => {
   return (
-<section className='main'>
+<section className={Mainstyle.main}>
     <div className="mainBody">
-        
+        <div className={Mainstyle.mainheader}>
+          <h2 className={Mainstyle.mainTitle}>Popular Destinaitons by Moove.</h2>
+          <div className={Mainstyle.mainright}>
+            <h4 className={Mainstyle.MainTtilealttext}><span className={Mainstyle.mainSpanTitle}>1000+</span> Destinaitons</h4>
+            <div className={Mainstyle.mainHdrAltText}>
+              <p className={Mainstyle.mainText}>These are the 4 most popular destinaitons today.There are 1k destinaitons waiting for you, let's finish it now!</p>
+            </div>
+          </div>
+        </div>
     </div>
 </section>
   )
