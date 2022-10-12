2db5db62b47ba7ab76f930c62d7cfe81169a43c4
Tarik Doganay <99910240+GoodGuyForReal@users.noreply.github.com>
2022-10-12 13:48:00 +0200

    n

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -12,7 +12,7 @@ const Main = () => {
           <div className={Mainstyle.mainright}>
             <h4 className={Mainstyle.MainTtilealttext}><span className={Mainstyle.mainSpanTitle}>1000+</span> Destinaitons</h4>
             <div className={Mainstyle.mainHdrAltText}>
-              <p className={Mainstyle.mainText}>These are the 4 most popular destinaitons today.There are 1k destinaitons waiting for you, let's finish it now!</p>
+              <p className={Mainstyle.mainText}>These are the most popular destinaitons today.There are 1k destinaitons waiting for you, let's finish it now!</p>
             </div>
           </div>
         </div>
