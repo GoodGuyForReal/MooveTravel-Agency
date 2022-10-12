909fd3f7d2d731702fb3e000137c07fc824fba1c
Tarik Doganay <99910240+GoodGuyForReal@users.noreply.github.com>
2022-10-12 12:34:44 +0200

    v

--- src/components/sectiontext/TextSection.jsx
+++ src/components/sectiontext/TextSection.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './TextSection.css'
 
+
 const TextSection = () => {
   return (
     <section className='textSection'>
       <div className="textSecBody">
         <h1 className='TextSecTitle'>
-          “The biggest adventure you can take is to live the life of your dreams”
+          “This doesn’t only have to be one of those quotes on travel alone. Life itself has a lot of potholes. Whether you are traveling or at home working at your job, you should definitely enjoy the journey.”
         </h1>
       </div>
     </section>
