dadf0b31f931f877dcc63fc30a84ba65f1cc1b85
Tarik Doganay <99910240+GoodGuyForReal@users.noreply.github.com>
2022-10-12 14:56:11 +0200

    n

--- src/components/mobileapp/Mobile.jsx
+++ src/components/mobileapp/Mobile.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import'./Mobile.css';
-
+import './Mobile.css';
+import Mobileimg from '../assets/MobileApp.png'
+ 
 const Mobile = () => {
-  return (
-    <div>Mobile</div>
-  )
+    return (
+        <section className='Mobile'>
+            <div className="MobileBody">
+                <div className='Mimgd'>
+                    <img src={Mobileimg} className='MobileImg' alt="" />
+                </div>
+                <div>
+                    <h1>Download and Get Your First Journey!</h1>
+                    <p>ownload the Moove.com mobile app for one-touch access to your next travel adventure. With the Moove.com mobile app you’ll get access to hidden features and special offers.</p>
+                </div>
+            </div>
+
+        </section>
+    )
 }
 
 export default Mobile
