c970ea3cb2b51e01cb8a3bf9fe716d80c4d50209
Tarik Doganay <99910240+GoodGuyForReal@users.noreply.github.com>
2022-10-13 12:19:03 +0200

    n

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import './Navbar.css';
 
 
 function MNavbar() {
   return (
-    <Navbar bg="none" expand="lg">
+    <Navbar className='nav' expand="lg">
       <Container>
-        <Navbar.Brand href="#home">MOOVE.</Navbar.Brand>
+        <Navbar.Brand className='brandtext' href="#home">Moove.</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link className='active' href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">About Us</Nav.Link>
-            <Nav.Link href="#link">Pricing</Nav.Link>
+          <Nav className="itemlist ms-auto ">
+            <Nav.Link className='navitem 'href="#home">Home</Nav.Link>
+            <Nav.Link className='navitem ' href="#link">About Us</Nav.Link>
+            <Nav.Link className='navitem ' href="#link">Pricing</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
